81348bed7b7074ac69c73afafb009329ee77289a
Hantek08 <hantek90@gmail.com>
2021-11-26 15:46:11 +0100

    Update AccountTests.cs

--- AccountTests.cs
+++ AccountTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xunit;
 using Lektion2;
 
@@ -63,30 +63,52 @@ namespace Lektion2.UnitTests
         }
 
         [Fact]
-        public void Transfer_FromToAccount()
+        public void RemoveAll_WhenCalled_RemovesAllTheMoneyFromAccount()
+        {
+            //Arrange
+            var account = new Account(new Kronor(10, 0));
+
+            //Act
+            account.RemoveAll();
+            var actualValue = account.Amount.KronorPart();
+
+            //Assert
+            Assert.Equal(0, actualValue);
+
+        }
+
+        [Fact]
+        public void Transfer_FromToAccount_ReturnAmount()
         {
             //Arrange
             var account = new Account();
             var kronor = new Kronor(11, 0);
-            var account1 = new Account(new Kronor(20,0));
+            var account1 = new Account(kronor);
             var account2 = new Account();
 
             Account.Transfer(account1, account2, kronor);
-            var actualValue1 = account2.Amount.KronorPart();
+            var actualValue1 = account1.Amount.KronorPart();
+            var actualValue2 = account2.Amount.KronorPart();
 
-            //Assert.True(account1.Amount.IsZero());
-            Assert.True(account2.Amount.IsPositive());
-
-            Assert.Equal(11, actualValue1);
-            //Assert.Equal(2, actualValue2);
+            Assert.Equal(0, actualValue1);
+            Assert.Equal(11, actualValue2);
         }
 
+        [Fact]
+        public void Transfer_FromToAccount_ReturnTrue()
+        {
+            //Arrange
+            var account = new Account();
+            var kronor = new Kronor(11, 0);
+            var account1 = new Account(kronor);
+            var account2 = new Account();
 
+            Account.Transfer(account1, account2, kronor);
 
+            Assert.True(account1.Amount.IsZero());
+            Assert.True(account2.Amount.IsPositive());
 
-
-
-
+        }
 
     }
 }
