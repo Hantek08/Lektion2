9dc7ecffe4707b2532ff4906ea131f0cb655b7fa
Hanna Tekie <hantek90@gmail.com>
2021-11-26 15:13:23 +0100

    Add project files.

---
+++ Account.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lektion2
+{
+    public class Account
+    {
+        /*
+         * Mängden pengar i kontot.
+         * Får vara negativt, men aldrig minska när det redan är negativt
+         * Exempel:
+         * 5 - 10 är tillåtet 
+         * -10 + 5 är tillåtet
+         * -1 + 7 är tillåtet
+         * -10 -5 är inte tillåtet
+         */
+        private Kronor amount = new Kronor();
+
+        public Kronor Amount
+        {
+            get => amount;
+            set => amount = value;
+
+        }
+
+        // Skapar ett tomt konto
+        public Account()
+        {
+
+        }
+
+        // Skapar ett konto med pengar i
+        public Account(Kronor money)
+        {
+            Deposit(money);
+        }
+
+        /* 
+         * Sätter in pengar på kontot.
+         */
+        public void Deposit(Kronor money)
+        {
+            amount = amount.Add(money);
+        }
+
+        /*
+         * Tar ut pengar ur kontot
+         * Minst 90% av pengarna måste finnas på kontot för att uttaget ska godkännas
+         */
+        public void Withdraw(Kronor money)
+        {
+            if (!(amount.Öre >= (money.Öre  * 90/100)))
+            {
+                throw new ArgumentException("Insufficient balance");
+            }  
+            amount = amount.Subtract(money);
+            
+           
+        }
+
+        /*
+         * Tar bort alla pengar ur plånboken
+         */
+        public void RemoveAll()
+        {
+            amount = new Kronor();
+        }
+
+        /*
+         * Överför pengar mellan två konton
+         * Pengarna ska tas bort från from-kontot och läggas till till to-kontot
+         * Övriga regler ska också upprätthållas
+         */
+        public static void Transfer(Account from, Account to, Kronor amount)
+        {
+                from.Withdraw(amount);
+                
+                 to.Deposit(amount);
+
+            //to.Deposit(to.amount);  // to = 13 from = 0
+            
+        }
+    }
+}
---
+++ Wallet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lektion2
+{
+    public class Wallet
+    {
+        // Mängden pengar i plånboken, får aldrig vara negativt
+        public Kronor amount = new Kronor();
+
+        // Skapar en tom plånbok
+        public Wallet()
+        {
+
+        }
+
+        // Skapar en plånbok med pengar i
+        public Wallet(Kronor money)
+        {
+            Add(money);
+        }
+
+        /* 
+         * Lägger till pengar till plånboken.
+         */
+        public void Add(Kronor money)
+        { 
+            amount = amount.Add(money);
+        }
+
+        /*
+         * Tar pengar ur plånboken
+         * Det ska inte gå att ta ut mer pengar än vad som finns i plånboken
+         */
+        public void Remove(Kronor money)
+        {
+            if (!(amount.Öre >= money.Öre))
+            {
+                throw new ArgumentException("Insufficient balance");
+            }
+            amount = amount.Subtract(money);
+        }
+
+        /*
+         * Tar bort alla pengar ur plånboken
+         */
+        public void RemoveAll()
+        {
+            amount = new Kronor();
+        }
+    }
+}
